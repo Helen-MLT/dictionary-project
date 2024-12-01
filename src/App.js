e152cf89912e9b4e72772411556073a97e529f4e
Helen-MLT <h.milletti@gmail.com>
2024-12-01 13:09:36 +0100

    Added links

--- src/App.js
+++ src/App.js
@@ -5,12 +5,18 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header"></header>
+        <header className="App-header">Dictionary</header>
         <main>
           <Dictionary />
         </main>
         <footer className="App-footer">
-          <small>Coded by Hélèna Milletti</small>
+          <small>
+            Coded by <a href="https://github.com/Helen-MLT">Hélèna Milletti</a>,
+            open sourced on{" "}
+            <a href="https://github.com/Helen-MLT/dictionary-project">Github</a>{" "}
+            and hosted on{" "}
+            <a href="https://dictionary-byhelen.netlify.app/">Netlify</a>
+          </small>
         </footer>
       </div>
     </div>
